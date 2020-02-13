625d33a84c93d96138777af3c3045af44754825f
manu <manu.villar.vitoria@gmail.com>
2020-02-13 18:53:55 +0100

    understanding & Using State

--- src/App.js
+++ src/App.js
@@ -1,54 +1,35 @@
-//import React from 'react';
 import React,{ Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-//function App() {
-
-  //return (
-   // <div className="App">
-      
-     // <h1> Hi, I'm React App </h1>
-    //</div>
-  //);
 
   class App extends Component{
+
+    state = {
+
+      persons:[
+       {name: 'Max', age: 28 },      
+       {name: 'Manu', age: 29 },
+       {name: 'Sthepanie', age: 26 }
+     
+      ]
+    }
+
     render(){
-
-      //return React.createElement('div', null, 'h1','Hi, I\'m React App');
-
-      /**** El h1 lo interpreta como un texto, por eso hay 
-            que crear el método React.createElement como vemos abajo para que 
-            el texto se vea en h1****/
-
-      //return React.createElement('div', null, React.createElement ('h1',null, 'Does this work now?'));
-
-      /**** Aquí al poner el null en vez del {className:'App'} como vemos abajo
-            el texto no queda centrado****/
-
-      //return React.createElement('div', {className:'App'}, React.createElement ('h1',null, 'Does this work now?'));
-
-
-      /**** Con el código de arriba obtenemos el mismo resultado que usando HTML dentro del javacript (jsx)
-             de forma más sencilla, sin tener que estar creando elementos, como podemos observar
-             en el código de abajo****/
 
       return (
         <div className="App">
           <h1> Hi, I'm React App </h1>
           <p>This is really working</p>
+          <button> Switch Name</button>
 
-          <Person name="Max" age="28"/> 
-          <Person name="Manu" age="29"> My hobbies:  Racing </Person> 
-          <Person name="Stephanie" age="26"/> 
+          <Person name= {this.state.persons[0].name} age= {this.state.persons[0].age}/> 
+          <Person name= {this.state.persons[1].name}  age= {this.state.persons[1].age}> My hobbies:  Racing </Person> 
+          <Person name= {this.state.persons[2].name}  age= {this.state.persons[2].age}/> 
           
         </div> 
         
-        /*** colocando el componente <Person/> que hemos importado, 
-        mostramos el texto que figura en el archivo Person.js ***/  
-      );
-
-
+      );      
     } 
 }
 
