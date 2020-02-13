1671e4eb9a4aae47b8945d138987f2538e04d87a
manu <manu.villar.vitoria@gmail.com>
2020-02-13 19:08:40 +0100

    Handling Events with Methods

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ import Person from './Person/Person';
       persons:[
        {name: 'Max', age: 28 },      
        {name: 'Manu', age: 29 },
-       {name: 'Sthepanie', age: 26 }
-     
+       {name: 'Sthepanie', age: 26 }    
       ]
     }
+
+    switchNameHandler = () =>{
+      console.log ("was clicked!");
+    }
+
 
     render(){
 
@@ -21,7 +25,7 @@ import Person from './Person/Person';
         <div className="App">
           <h1> Hi, I'm React App </h1>
           <p>This is really working</p>
-          <button> Switch Name</button>
+          <button onClick= {this.switchNameHandler}>Switch Name</button>
 
           <Person name= {this.state.persons[0].name} age= {this.state.persons[0].age}/> 
           <Person name= {this.state.persons[1].name}  age= {this.state.persons[1].age}> My hobbies:  Racing </Person> 
