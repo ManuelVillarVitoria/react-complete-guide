cf75280766f9fcd1c9687ea8b5060ed7844a9205
manu <manu.villar.vitoria@gmail.com>
2020-02-13 15:25:17 +0100

    understanding jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+//import React from 'react';
+import React,{ Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//function App() {
+
+  //return (
+   // <div className="App">
+      
+     // <h1> Hi, I'm React App </h1>
+    //</div>
+  //);
+
+  class App extends Component{
+    render(){
+
+      //return React.createElement('div', null, 'h1','Hi, I\'m React App');
+
+      /**** El h1 lo interpreta como un texto, por eso hay 
+            que crear el método React.createElement como vemos abajo para que 
+            el texto se vea en h1****/
+
+      //return React.createElement('div', null, React.createElement ('h1',null, 'Does this work now?'));
+
+      /**** Aquí al poner el null en vez del {className:'App'} como vemos abajo
+            el texto no queda centrado****/
+
+      //return React.createElement('div', {className:'App'}, React.createElement ('h1',null, 'Does this work now?'));
+
+
+      /**** Con el código de arriba obtenemos el mismo resultado que usando HTML dentro del javacript (jsx)
+             de forma más sencilla, sin tener que estar creando elementos, como podemos observar
+             en el código de abajo****/
+
+      return (
+        <div className="App">
+          <h1> Hi, I'm React App </h1>
+          <p>This is really working</p>
+        </div> 
+      );
+
+
+    } 
 }
 
 export default App;
