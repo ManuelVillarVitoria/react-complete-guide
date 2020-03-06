516062b9c9700ac1f0bc3dbcb3f825a23b51f5ed
manu <manu.villar.vitoria@gmail.com>
2020-03-06 15:31:48 +0100

    Component Creation Lifecycle in Action

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,95 +1,96 @@
 import React, { Component } from 'react';
 
-import classes from  './App.module.css';
+import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
 
   state = {
-
     persons: [
-      { id:'asfa1', name: 'Max', age: 28 },
-      { id:'vasdf1', name: 'Manu', age: 29 },
-      { id:'asdf11', name: 'Stephanie', age: 26 }
+      { id: 'asfa1', name: 'Max', age: 28 },
+      { id: 'vasdf1', name: 'Manu', age: 29 },
+      { id: 'asdf11', name: 'Stephanie', age: 26 }
     ],
-
     otherState: 'some other value',
+    showPersons: false
+  };
 
-    showPersons:false
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentWillMount() {
+    console.log('[App.js] componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
   }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id; 
-    }); 
+      return p.id === id;
+    });
 
     const person = {
       ...this.state.persons[personIndex]
     };
 
-    /***Aquí vemos una alternativa menos moderna para 
-    hacer lo mismo que con "..." ***/
+    // const person = Object.assign({}, this.state.persons[personIndex]);
 
-    //const person = Object.assign({}, this.state.persons[personIndex]);
-    
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState( {persons: persons} )
+    this.setState({ persons: persons });
+  };
 
-    } 
-  
-
-  deletePersonHandler = (personIndex) => {
-
-    //const persons = this.state.persons.slice();
-
-    /***Ahora mostramos un método similar al "slice()" 
-    pero más moderno (ES6) que es "[...   ]"***/
-
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-
-    this.setState({ persons: persons })
-  }
+    this.setState({ persons: persons });
+  };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState ({showPersons: !doesShow});
-  }
+    this.setState({ showPersons: !doesShow });
+  };
 
-  render () {
-
+  render() {
+    console.log('[App.js] render');
     let persons = null;
-  
-    if (this.state.showPersons) {
 
-      persons = <Persons
-          persons= {this.state.persons}
-          clicked= {this.deletePersonHandler}
-          changed= {this.nameChangedHandler} />;
+    if (this.state.showPersons) {
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
+      );
     }
 
     return (
-
-      <div className= {classes.App}>
+      <div className={classes.App}>
         <Cockpit
-          title= {this.props.appTitle}
-          showPersons= {this.state.showPersons}
-          persons= {this.state.persons}
-          clicked= {this.togglePersonsHandler}/>
+          title={this.props.appTitle}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
-
     );
-
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
-
 }
-
 
 export default App;
